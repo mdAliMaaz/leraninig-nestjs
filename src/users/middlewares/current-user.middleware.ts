e00a674b38bc06fce95e99430af6b46b4d9bc44c
maaz <mdmaazi2907@gmail.com>
2024-03-22 15:58:25 +0530

    changes

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,8 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { Request, Response, NextFunction } from 'express';
+import { User } from '../users.entity';
 
 // to use DI we need to mark the class with @Injectable() and create a private property of userService in constructor of the class like this constructor(private userService: UsersService) {}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -12,13 +21,16 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (id) {
       const user = await this.userService.findOne(id);
+      req.currentUser = user;
     }
-    // @ts-ignore
-    req.currentUser = user;
   }
 }
 
-// in order to use middleware globally we need to configure them in user Module like this export class UsersModule {
+// @Transform() to transform the value youcan use this on column in entity class
+
+//Reposortry is generated automatically by 'TyprOrm' 
+
+// in order to use middleware globally we need to configure them in user Module like this export class       UsersModule {
 //   configure(consumer: MiddlewareConsumer) {
 //     consumer.apply(CurrentUserInterceptor).forRoutes('*');
 //   }
